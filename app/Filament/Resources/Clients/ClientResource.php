0207213577a0f6612816f40a910f3a8b53fea82d
txyxter <ramses.tchamou@kimbocare.com>
2025-08-31 21:13:25 -0400

    add docker file

--- app/Filament/Resources/Clients/ClientResource.php
+++ app/Filament/Resources/Clients/ClientResource.php
@@ -32,9 +32,7 @@ class ClientResource extends Resource
 
     public static function getRelations(): array
     {
-        return [
-            //
-        ];
+        return [];
     }
 
     public static function getPages(): array
